c86a2190b46177bd59f968fd98a8efbe6472aeeb
Mohamed Fazil M <fazilmm860@gmail.com>
2023-05-25 15:27:05 +0530

    App.js,data.js, Table folder created and UserTable.jsx

---
+++ src/data.js
@@ -0,0 +1,14 @@
+const userList = [
+    {
+        id: 1,
+        name: 'Adiyan',
+        username: 'Adiyan Ahamed'
+    },
+    {
+        id: 2,
+        name: 'Aneeq',
+        username: 'Aneeq Ahamed'
+    }
+];
+
+export default userList;
